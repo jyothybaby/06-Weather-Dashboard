ea7bc3dd1bdd3a06143916eabe4357f74b2623aa
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-07-29 00:29:19 -0400

    Displaying temperature, wind,humidity and UVIndex in the main card area

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var displayLocationEl = document.querySelector(".display-location");
+var temperatureEl = document.querySelector("#temperature");
+var windEl = document.querySelector("#wind");
+var humidityEl = document.querySelector("#humidity");
+var UVIndexEl = document.querySelector("#UVIndex");
+
+
 var APIKey = "363823c2e92dad51019d30b82e6c13d8";
 var citiesArray = [];
 function initializeCities() {
@@ -65,6 +71,7 @@ searchBtn.addEventListener("click", function (event) {
 })
 
 function getWeatherInfo(cityEl) {
+    // used my key = 363823c2e92dad51019d30b82e6c13d8
     var apiurl = "https://api.openweathermap.org/data/2.5/weather?q= "+ cityEl + "&appid=363823c2e92dad51019d30b82e6c13d8";
     fetch(apiurl)
     .then(function(response){
@@ -73,9 +80,39 @@ function getWeatherInfo(cityEl) {
     .then(function (data){
         console.log(data);
         var day = moment().format("MMM Do YYYY"); 
-        displayLocationEl.textContent = data.name + "- "+ day;
-        console.log("displayLocationEl");
+        displayLocationEl.textContent = data.name + "- "+ day  ;
+        //var iconCode = data.weather[0].icon;
+       // var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+      //  $('#wicon').attr('src', iconurl);
+        // icon is pending
+        var temp= data.main.temp;
+        console.log(temp);
+        var temp1= ((temp-273.15)*1.8)+32;
+        console.log(temp1)
+       var temp2 = Math.round(temp1);
+       console.log(temp2)
+       temperatureEl.textContent = "Temperature : " + temp2;
+        windEl.textContent = "Wind: "+ data.wind.speed;
+        humidityEl.textContent = "Humidity: "+ data.main.humidity;
+        var latt = data.coord.lat;
+        console.log(latt)
+        var lonn = data.coord.lon;
+        console.log(lonn)
+
+var uvIndexUrl = "https://api.openweathermap.org/data/2.5/uvi?appid=363823c2e92dad51019d30b82e6c13d8&lat=" +latt + "&lon="+lonn;
+
+fetch(uvIndexUrl)
+.then(function(response){
+    return response.json();
+})
+.then(function(info){
+    console.log(info);
+    UVIndexEl .textContent = "UVIndex: "+ info.value;
+
+})
+        
     })
+    
 }
 
 var removeCities = document.getElementById("clear-city");
